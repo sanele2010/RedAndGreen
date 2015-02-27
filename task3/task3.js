04c653a31d4fd41ccb3a8d739c1578539c266392
sanele <sanele@projectcodex.co>
2015-02-27 14:54:37 +0200

    3

---
+++ task3/task3.js
@@ -0,0 +1,12 @@
+function myFunction()
+{
+  document.getElementById("square").style.backgroundColor="red"; 
+ 
+}
+
+function myFunction1()
+{
+  document.getElementById("square").style.backgroundColor="green"; 
+
+}
+
